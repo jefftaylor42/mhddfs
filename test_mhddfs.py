257480026875a68a8de62fae796c2dccf2ad875f
Jeff Taylor <jtaylor@3vgeomatics.com>
2016-01-08 14:45:17 -0800

    Restructure setup/cleanup

--- test_mhddfs.py
+++ test_mhddfs.py
@@ -1,14 +1,34 @@
 #!/usr/bin/env python3
 import pytest
+import os, shutil, subprocess
+import time
 
 def sh(cmd):
     import subprocess
     subprocess.check_call(cmd, shell=True)
 
-def test_readdir():
-    import os, shutil, subprocess
-    import time
-    # Create directories
+def test_readdir(mhddfs):
+    # Touch some files.
+    sh("touch test1/1")
+    sh("touch test1/2")
+    sh("touch test1/3")
+    sh("touch test2/4")
+    sh("touch test2/5")
+    sh("touch test2/6")
+
+    # ls the aggregated directory to show they're all there.
+    assert set(os.listdir("mnt")) == {'1', '2', '3', '4', '5', '6'}
+
+def assert_valgrind():
+    """Check that valgrind hasn't detected errors."""
+    # valgrind XML is invalid due to fork() in mhddfs (valgrind bug)
+    ret = subprocess.call("grep -q '<error>' valgrind-out.xml", shell=True)
+    if ret == 0:
+        # grep found a match
+        raise AssertionError("Memory errors in tested program")
+
+@pytest.fixture
+def mhddfs(request):
     sh("rm -rf test1 test2 mnt")
     os.mkdir("test1")
     os.mkdir("test2")
@@ -19,25 +39,9 @@ def test_readdir():
                     shell=True)
     time.sleep(1) # Apparantly mhddfs takes some time to start.
 
-    # Touch some files.
-    sh("touch test1/1")
-    sh("touch test1/2")
-    sh("touch test1/3")
-    sh("touch test2/4")
-    sh("touch test2/5")
-    sh("touch test2/6")
+    def cleanup():
+        sh("fusermount -uz mnt")
+        sh("rm -rf test1 test2 mnt")
+        assert_valgrind()
 
-    # Do some ls-ing
-    assert set(os.listdir("mnt")) == {'1', '2', '3', '4', '5', '6'}
-    sh("fusermount -u mnt")
-
-    # valgrind XML is invalid due to fork() in mhddfs (valgrind bug)
-    ret = subprocess.call("grep -q '<error>' valgrind-out.xml", shell=True)
-    if ret == 0:
-        # grep found a match
-        raise AssertionError("Memory errors in tested program")
-
-@pytest.fixture
-def cleanup(request):
-    request.addfinalizer(lambda: subprocess.call("fusermount -uz mnt"))
-
+    request.addfinalizer(cleanup)
